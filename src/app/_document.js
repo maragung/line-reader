f81e61987d6fe7ca8bd485a6912fe7351624e1d7
Maragung <50156713+maragung@users.noreply.github.com>
2025-03-28 21:42:01 +0700

    Create _document.js

---
+++ src/app/_document.js
@@ -0,0 +1,21 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <title>Line Reader</title>
+          <meta name="description" content="Read and navigate lines from a text file" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
